2a353407775514ac588db892e4b62d244f8fcf89
mkdlufop <mkdlufop@qq.com>
2022-06-23 23:58:49 +0800

    add: 补充首页用到的其他组件

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,15 +1,34 @@
 <template>
     <div>
         <TypeNav />
+        <ListContainer />
+        <TodayRecommend />
+        <GoodsRank />
+        <GuessYouLike />
+        <TheFloor />
+        <TheFloor />
+        <TheBrand />
     </div>
 </template>
 
 <script>
 import TypeNav from './TypeNav'
+import ListContainer from './ListContainer'
+import TodayRecommend from './TodayRecommend'
+import GoodsRank from './GoodsRank'
+import GuessYouLike from './GuessYouLike'
+import TheFloor from './TheFloor'
+import TheBrand from './TheBrand'
 
 export default {
     components: {
-        TypeNav,        
+        TypeNav,
+        ListContainer,
+        TodayRecommend,
+        GoodsRank,
+        GuessYouLike,
+        TheFloor,
+        TheBrand,
     },
 }
 </script>
