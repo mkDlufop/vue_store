f94c29aa4a5e9db9c671cf89c401fcb8c69c38e5
mkdlufop <mkdlufop@qq.com>
2022-06-23 14:30:06 +0800

    增加：添加HomeHeader和HomeFooter组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <HomeHeaderVue></HomeHeaderVue>
+    <HomeFooterVue></HomeFooterVue>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import HomeHeaderVue from './components/HomeHeader'
+import HomeFooterVue from './components/HomeFooter'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    HomeHeaderVue,
+    HomeFooterVue
   }
 }
 </script>
@@ -23,6 +29,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
