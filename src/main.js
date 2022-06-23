2876b139b2f4d9a3601f75d98a73aa76dd15bb04
mkdlufop <mkdlufop@qq.com>
2022-06-23 15:08:47 +0800

    配置：引入vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import router from './router'
+
 Vue.config.productionTip = false
 
 new Vue({
+  router, // 注册路由
   render: h => h(App),
 }).$mount('#app')
