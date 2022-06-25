05c5c5a892acc57136b27da56fd81eea08efd574
mkdlufop <mkdlufop@qq.com>
2022-06-25 22:11:16 +0800

    add: 引入进度条插件nprogress

--- src/api/request.js
+++ src/api/request.js
@@ -2,6 +2,10 @@
 
 import axios from "axios";
 
+// 引入nprogress及相关样式
+import nprogress from "nprogress";
+import "nprogress/nprogress.css"
+
 // 利用create方法创建一个axios实例
 const requests = axios.create({
     baseURL: '/api', // 基础路径
@@ -11,6 +15,7 @@ const requests = axios.create({
 // 请求拦截器
 // 发送请求前可以做一些事情，如：检查权限、增加页面Loading、网络状态判断等
 requests.interceptors.request.use((config) => {
+    nprogress.start();
     return config;
 }, (error) => {
     return Promise.reject(error);
@@ -19,6 +24,7 @@ requests.interceptors.request.use((config) => {
 // 响应拦截器
 requests.interceptors.response.use((res) => {
     // 成功的回调函数
+    nprogress.done();
     return res.data;
 },(error) => {
     // 失败的回调函数
