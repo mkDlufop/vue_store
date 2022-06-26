7594b57c12cf2385ab7f06523224f920283dc3d9
mkdlufop <mkdlufop@qq.com>
2022-06-26 20:39:52 +0800

    add: 完成三级联动组件TypeNav动态背景颜色

--- src/components/TypeNav/index.vue
+++ src/components/TypeNav/index.vue
@@ -171,6 +171,9 @@ export default {
                         }
                     }
                 }
+                .item:hover{
+                    background: skyblue;
+                }
             }
         }
     }
