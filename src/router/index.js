f37b013b31a8260224f8ab0a12f96fa7f42f3a91
mkDlufop <mkdlufop@qq.com>
2023-03-05 22:51:54 +0800

    update: 利用导航守卫使用户登录后不可访问登录注册页面、刷新页面后当前页面保留用户信息的数据

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
+import routes from './routes'
+import store from '@/store'
+
 // 重写push和replace方法
 // 解决以下问题：
 //    编程式路由跳转到当前路由（参数不变），多次执行会抛出NavigationDuplicated的警告错误。
@@ -26,75 +29,46 @@ VueRouter.prototype.replace = function (location, resolve, reject) {
   }
 }
 
-import Home from '@/views/Home'
-import Login from '@/views/Login'
-import Register from '@/views/Register'
-import Search from '@/views/Search'
-import Detail from '@/views/Detail'
-import AddCartSuccess from '@/views/AddCartSuccess'
-import ShopCart from '@/views/ShopCart'
-
-export default new VueRouter({
-  routes: [
-    {
-      path: '/home',
-      component: Home,
-      meta: {
-        HomeFooterShow: true,
-      }
-    },
-    {
-      path: '/login',
-      component: Login,
-      meta: {
-        HomeFooterShow: false,
-      }
-    },
-    {
-      path: '/register',
-      component: Register,
-      meta: {
-        HomeFooterShow: false,
-      }
-    },
-    {
-      path: '/search/:keyword?', // 添加?，可指定params参数可传可不传
-      name: 'search',
-      component: Search,
-      meta: {
-        HomeFooterShow: true,
-      }
-    },
-    {
-      path: '/detail/:skuid',
-      component: Detail,
-      meta: {
-        HomeFooterShow: true,
-      }
-    },
-    {
-      path: '/addcartsuccess',
-      name: 'addcartsuccess',
-      component: AddCartSuccess,
-      meta: {
-        HomeFooterShow: true,
-      }
-    },
-    {
-      path: '/shopcart/',
-      component: ShopCart,
-      meta: {
-        HomeFooterShow: true,
-      }
-    },
-    {
-      // 重定向，访问/时，重定向到首页
-      path: '*',
-      redirect: '/home',
-    }
-  ],
+// 创建 VueRouter 类的实例
+let router = new VueRouter({
+  routes,
   // 对于所有路由导航，简单地让页面滚动到顶部
   scrollBehavior() {
     return { x: 0, y: 0 }
   }
 })
+
+// 全局前置守卫
+router.beforeEach(async (to, from, next) => {
+  let token = store.state.user.token;
+  let name = store.state.user.userInfo.name;
+
+  if (token) {
+    // 用户已登录
+    if (to.path == "/login" || to.path == "/register") {
+      // 路由跳转到登录页
+      next("/");
+    } else {
+      // 路由跳转到其他
+      if (name) {
+        next();
+      } else {
+        // 发送请求获取用户信息
+        try {
+          await store.dispatch("getUserInfo");
+          next();
+        } catch (error) {
+          // token 失效了，需要重新登录
+          await store.dispatch("userLogout");
+          next("/login");
+        }
+      }
+    }
+  } else {
+    // 用户未登录
+    next();
+  }
+})
+
+// 对外暴露 VueRouter 类的实例
+export default router;
---
+++ src/router/routes.js
@@ -0,0 +1,66 @@
+import Home from '@/views/Home'
+import Login from '@/views/Login'
+import Register from '@/views/Register'
+import Search from '@/views/Search'
+import Detail from '@/views/Detail'
+import AddCartSuccess from '@/views/AddCartSuccess'
+import ShopCart from '@/views/ShopCart'
+
+export default [
+  {
+    path: '/home',
+    component: Home,
+    meta: {
+      HomeFooterShow: true,
+    }
+  },
+  {
+    path: '/login',
+    component: Login,
+    meta: {
+      HomeFooterShow: false,
+    }
+  },
+  {
+    path: '/register',
+    component: Register,
+    meta: {
+      HomeFooterShow: false,
+    }
+  },
+  {
+    path: '/search/:keyword?', // 添加?，可指定params参数可传可不传
+    name: 'search',
+    component: Search,
+    meta: {
+      HomeFooterShow: true,
+    }
+  },
+  {
+    path: '/detail/:skuid',
+    component: Detail,
+    meta: {
+      HomeFooterShow: true,
+    }
+  },
+  {
+    path: '/addcartsuccess',
+    name: 'addcartsuccess',
+    component: AddCartSuccess,
+    meta: {
+      HomeFooterShow: true,
+    }
+  },
+  {
+    path: '/shopcart/',
+    component: ShopCart,
+    meta: {
+      HomeFooterShow: true,
+    }
+  },
+  {
+    // 重定向，访问/时，重定向到首页
+    path: '*',
+    redirect: '/home',
+  }
+]
