de1dbe60b0d42299ffe9dec747400c18313df995
mkDlufop <mkdlufop@qq.com>
2023-03-06 21:42:53 +0800

    update: 修改 vue.config.js 使项目打包后不产生 map 文件

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  // productionSourceMap 为 false 表示项目打包后不产生 map 文件
+  // （有了 map 的话，运行打包后的项目会准确的输出是哪行哪列出错）
+  productionSourceMap: false,
   transpileDependencies: true,
   lintOnSave: false, // 关闭eslint
   devServer: { // 代理跨域
